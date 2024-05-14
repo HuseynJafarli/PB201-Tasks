6713b39667c84223da10ab4ed33473d6ae2ed525
Huseyn Jafarli <hjeferli18088@ada.edu.az>
2024-05-14 10:38:43 +0400

    All tasks done

---
+++ TestProgram/TestProgram/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.Intrinsics.X86;
+using System.Runtime.Serialization.Formatters;
+
+namespace TestProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int cnt = 0;
+
+
+            //Task 1
+
+            //    bool isPrime = IsPrime(n);
+
+            //    if (isPrime)
+            //    {
+            //        Console.WriteLine(n + " is a prime number.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(n + " is a composite number.");
+            //    }
+            //}
+
+            //static bool IsPrime(int n)
+            //{
+            //    if (n <= 1) return false;
+            //    for (int i = 2; i <= Math.Sqrt(n); i++)
+            //    {
+            //        if (n % i == 0)
+            //        {
+            //            return false;
+            //        }
+            //    }
+            //    return true;
+            //}
+
+            //Task 2
+            while(n > 0)
+            {
+                n /= 10;
+                cnt++;
+            }
+            Console.WriteLine(cnt);
+        }
+
+    }
+}
