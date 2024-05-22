8a3bd3b93d574baac835e0ea5c9a0a5a4cd31c53
Huseyn Jafarli <hjeferli18088@ada.edu.az>
2024-05-22 11:27:11 +0400

    Car class created , Resize method created.

---
+++ CarClassAndResize/ConsoleApp1/Car.cs
@@ -0,0 +1,28 @@
+﻿namespace ConsoleApp1
+{
+    public class Car
+    {
+        public string Brand;
+        public string Model;
+        public double CurrentFuel;
+        public double FuelPerKm;
+        public double Millage;
+
+        public void Drive(int km)
+        {
+            double requiredFuel = km * FuelPerKm;
+
+            if (CurrentFuel >= requiredFuel)
+            {
+                Millage += km;
+                CurrentFuel -= requiredFuel;
+                Console.WriteLine($"{Brand} {Model} have driven {km}km . Millage: {Millage}km. Remaining fuel: {CurrentFuel} liters.");
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel.");
+            }
+        }
+
+    }
+}
---
+++ CarClassAndResize/ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] originalArr = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine($"Original array length: {originalArr.Length}");
+
+            int[] resizedArr = ArrayResize(originalArr, 7);
+
+            Console.WriteLine($"Resized array length: {resizedArr.Length}");
+
+
+            //Car car = new Car();
+            //car.Brand = "Ford";
+            //car.Model = "Mustang";
+            //car.CurrentFuel = 60;
+            //car.FuelPerKm = 0.2;
+            //car.Drive(50);
+            //car.Drive(100);
+            //car.Drive(500);
+        }
+        public static int[] ArrayResize(int[] array, int newSize)
+        {
+            if (newSize < 0)
+                Console.WriteLine("Error occured.");
+
+            int[] newArray = new int[newSize];
+            int existingElements;
+            if (array.Length < newSize)
+            
+                existingElements = array.Length;
+            
+            else
+                existingElements = newSize;
+
+            for (int i = 0; i < existingElements; i++)
+            
+                newArray[i] = array[i];
+            
+
+            return newArray;
+        }
+
+    }
+}
